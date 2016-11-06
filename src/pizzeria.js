e9e2f6979cb3821947fcbebfc3edabf0a6afa9ef
Rynaret <sun.re@bk.ru>
2016-11-06 17:35:31 +0300

    When order is 2 standard pizza then order amount is 90% of the sum of 2 standard pizzas

--- src/pizzeria.js
+++ src/pizzeria.js
@@ -1,15 +1,24 @@
 export function Pizzeria(standardPizzaPrice) {
     this.standardPizzaPrice = standardPizzaPrice;
 
-    this.calculatePrice = (pizza)=>{
-
-        let pizzaComposition = pizza.composition;
+    this.calculatePrice = (pizzas)=>{
         let pizzaPrice = 0;
 
-        pizzaPrice += pizza.isStandard() ? this.standardPizzaPrice : 0;
+        let standardPizzas = pizzas.filter(x => x.isStandard());
+        let otherPizzas = pizzas.filter(x => !x.isStandard());
 
-        if(pizzaComposition.dough === 'thin' && pizzaComposition.baseFilling === 'tomato'){
-            pizzaPrice += this.standardPizzaPrice;
+        otherPizzas.forEach(pizza =>{
+            let pizzaComposition = pizza.composition;
+
+            if(pizzaComposition.dough === 'thin' && pizzaComposition.baseFilling === 'tomato'){
+                pizzaPrice += this.standardPizzaPrice;
+            }
+        });
+
+        if(standardPizzas.length > 1){
+            pizzaPrice += this.standardPizzaPrice * standardPizzas.length * 0.9;
+        }else{
+            pizzaPrice += this.standardPizzaPrice * standardPizzas.length;
         }
 
         return pizzaPrice;
--- tests/pizzaTests.js
+++ tests/pizzaTests.js
@@ -4,18 +4,19 @@ import { DslPizza } from '../src/dslPizza'
 import { DslPizzeria } from '../src/dslPizzeria'
 
 
-suite('When order', ()=>{
+suite('When standard Pizza Price is 2 dollars and order', ()=>{
     const standardPizzaPrice = 2;
     let pizzeria = new DslPizzeria()
         .withStandardPizzaPrice(standardPizzaPrice)
         .build();
 
-    suite('is standard pizza and standard Pizza Price is 2 dollars', ()=>{
+    suite('is standard pizza', ()=>{
         test('then order amount is 2 dollars', ()=>{
             let pizza = new DslPizza()
                 .buildStandard();
+            let order = [pizza];
 
-            const orderAmount = pizzeria.calculatePrice(pizza);
+            const orderAmount = pizzeria.calculatePrice(order);
 
             assert.equal(standardPizzaPrice, orderAmount);
         });
@@ -27,10 +28,23 @@ suite('When order', ()=>{
                 .withBaseFilling('thin')
                 .withBaseFilling('tomato')
                 .build();
+            let order = [pizza];
 
-            const orderAmount = pizzeria.calculatePrice(pizza);
+            const orderAmount = pizzeria.calculatePrice(order);
 
             assert.equal(standardPizzaPrice, orderAmount);
+        });
+    });
+
+    suite('is 2 standard pizza', ()=>{
+        test('then order amount is 90% of the sum of 2 standard pizzas', ()=>{
+            let pizza = new DslPizza()
+                .buildStandard();
+            let order = [pizza, pizza];
+
+            const orderAmount = pizzeria.calculatePrice(order);
+
+            assert.equal(standardPizzaPrice * order.length * 0.9, orderAmount);
         });
     });
 });
